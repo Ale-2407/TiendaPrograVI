b9cb2aa410186fc74485d50655600a6b0950a442
Juan Carlos Nuñez Chavarria <134462352+JuankChavarria@users.noreply.github.com>
2025-06-22 11:40:24 -0600

    Agregar archivos de proyecto.

---
+++ Tienda-PrograVI/Tienda-PrograVI/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tienda_PrograVI.Models
+{
+    public class Producto
+    {
+        [Key]
+       public int Id_producto { get; set; }
+       public string Nombre { get; set; }
+       public string Descripcion { get; set; }
+       public double Precio { get; set; }
+       public int Stock { get; set; }
+       public int  Id_Categoria {  get; set; }
+
+
+    }
+}
